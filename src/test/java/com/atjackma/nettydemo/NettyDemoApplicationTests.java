11b2cae403635c5af300f12e7a5ce61ddc5c4ff9
hui <3457694954@qq.com>
2022-06-06 12:01:12 +0800

    Initial commit

---
+++ src/test/java/com/atjackma/nettydemo/NettyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atjackma.nettydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
